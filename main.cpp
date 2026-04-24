632b1039045de91fb0c34d83f6d58bb9a7a313f2
Julian69674 <julian.obergantschni@bulme.at>
2026-04-24 09:57:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include <Bounce2.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
 
 enum STATES {
   STATE_INVALID = 0,
@@ -10,12 +14,8 @@ enum STATES {
 
 const int BTN_PROD_01 = 4;
 const int BTN_PROD_02 = 3;
-const int BTN_MONEY = 2;
-const int LED_GRN = 13;
-const int LED_YLW = 12;
-const int LED_BLU = 11;
 const int MOTOR_RUNNING_TIME = 3000;
-const int WAIT_FOR_COIN_INSERTION_TIME = 4000;
+const int WAIT_FOR_FINAL_DECISION = 4000;
 const int MotorA1 = 10;
 const int MotorA2 = 9;
 const int MotorB1 = 8;
@@ -45,19 +45,12 @@ void setup() {
   button_prod_2.interval(10);
   button_prod_2.setPressedState(LOW);
 
-  button_money.attach(BTN_MONEY, INPUT_PULLUP);
-  button_money.interval(10);
-  button_money.setPressedState(LOW);
 
-  pinMode(LED_GRN, OUTPUT);
-  pinMode(LED_YLW, OUTPUT);
-  pinMode(LED_BLU, OUTPUT);
   pinMode(MotorA1, OUTPUT);
   pinMode(MotorA2, OUTPUT);
   pinMode(MotorB1, OUTPUT);
   pinMode(MotorB2, OUTPUT);
 
-  digitalWrite(LED_GRN, HIGH);
   current_state = STATES::STATE_IDLE;
 
   time1 = millis();
@@ -67,7 +60,7 @@ void setup() {
 void loop() {
   button_prod_1.update();
   button_prod_2.update();
-  button_money.update();
+
 
   switch (current_state) {
     case STATES::STATE_IDLE:
@@ -94,8 +87,6 @@ void HandleStateIdle()
   if(btn_prod_1_pressed)
   {
     current_state = STATES::STATE_SELECTED;
-    digitalWrite(LED_GRN, LOW);
-    digitalWrite(LED_YLW, HIGH);
     selected_product = 1;
     time1 = millis();
   }
@@ -103,8 +94,6 @@ void HandleStateIdle()
   if(btn_prod_2_pressed)
   {
     current_state = STATES::STATE_SELECTED;
-    digitalWrite(LED_GRN, LOW);
-    digitalWrite(LED_YLW, HIGH);
     selected_product = 2;
     time1 = millis();
   }
@@ -115,19 +104,21 @@ void HandleStateIdle()
 }
 void HandleStateSelected()
 {
-  bool btn_money_pressed = button_money.pressed();
-  if (millis() - time1 >= WAIT_FOR_COIN_INSERTION_TIME)
+  bool btn_prod_1_pressed = button_prod_1.pressed();
+  bool btn_prod_2_pressed = button_prod_2.pressed();
+  if (millis() - time1 >= WAIT_FOR_FINAL_DECISION)
   {
     time1 = millis();
-    digitalWrite(LED_GRN, HIGH);
-    digitalWrite(LED_YLW, LOW);
     current_state = STATES::STATE_IDLE;
   }
-  if(btn_money_pressed)
+  if(btn_prod_1_pressed)
   {
     current_state = STATES::STATE_MOTOR_ACTIVE;
-    digitalWrite(LED_YLW, LOW);
-    digitalWrite(LED_BLU, HIGH);
+    time1 = millis();
+  }
+  if(btn_prod_2_pressed)
+  {
+    current_state = STATES::STATE_MOTOR_ACTIVE;
     time1 = millis();
   }
 
@@ -137,8 +128,6 @@ void HandleStateSMotorActive()
   if (millis() - time1 >= MOTOR_RUNNING_TIME)
   {
     time1 = millis();
-    digitalWrite(LED_GRN, HIGH);
-    digitalWrite(LED_BLU, LOW);
     current_state = STATES::STATE_IDLE;
     
   }
